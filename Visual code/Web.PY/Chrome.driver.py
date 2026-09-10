3aa7679f7f8c21dab87a1d8de1d30f72df35c0cf
Lewis <lewis.chiu@etica.tw>
2026-09-10 15:05:19 +0800

    add Proj 【FTO01】

--- Visual code/Web.PY/Chrome.driver.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(driver_path=ChromeDriverManager().install(), options=options)  # ✅ 修正 `driver_path`
-
